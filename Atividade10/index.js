b5ea53973f1c17e901cc6efece98c6c2fe1d6b19
vi2511 <40776935+vi2511@users.noreply.github.com>
2022-05-11 22:13:33 -0300

    Add files via upload

--- Atividade10/index.js
+++ Atividade10/index.js
@@ -1,99 +1,20 @@
-function Retangulo(base, altura) {
-  this.base = base;
-  this.altura = altura;
-  this.Area = Area;
+var Alunos1 = new Object();
+Alunos1.propriedade = new Object();
+Alunos1.propriedade.ra = "000304813011";
+Alunos1.propriedade.nome = "Pedro";
 
-  function Area() {
-    alert("Área do Retângulo: " + this.base * this.altura);
+console.log(Alunos1)
+
+var Alunos2 = {
+  propriedade: {
+    ra: "0003048183010",
+    nome: "Rafael"
   }
 }
 
-var retangulo = new Retangulo(15, 30);
-retangulo.Area();
+console.log(Alunos2)
 
-  function Conta(){
-  var correntista, banco, conta, saldo;
-  this.getCorrentista = function(){
-    return correntista;
-  }
-  this.getBanco = function(){
-    return banco;
-  }
-  this.getConta = function(){
-    return conta;
-  }
-  this.getSaldo = function(){
-    return saldo;
-  }
-  this.setCorrentista = function(value){
-    correntista = value;
-  }
-  this.setBanco = function(value){
-    banco = value;
-  }
-  this.setConta = function(value){
-    conta = value;
-  }
-  this.setSaldo = function(value){
-    saldo = value;
-  }
-}
+var Alunos3 = {}
+Alunos3 = { ...Alunos3, propriedade: { ra: "0003048183010", nome: "Marcelo" } }
 
-function Corrente(){
-  var saldoEspecial;
-  this.getSaldoEspecial = function () {
-    return saldoEspecial;
-  }
-  this.setSaldoEspecial = function (value) {
-    saldoEspecial = value;
-  }
-}
-
-function Poupanca(){
-  var juros, dataVencimento;
-  this.getJuros = function () {
-    return juros;
-  }
-  this.getDataVencimento = function () {
-    return dataVencimento;
-  }
-  this.setJuros = function (value) {
-    juros = value;
-  }
-  this.setDataVencimento = function (value) {
-    dataVencimento = value;
-  }
-}
-
-Corrente.prototype = new Conta();
-Poupanca.prototype = new Conta();
-
-contaCorrente = new Corrente();
-contaCorrente.setCorrentista("Rafael Pereira Araujo");
-contaCorrente.setBanco("Banco do Brasil");
-contaCorrente.setConta("180154-0");
-contaCorrente.setSaldo(2500);
-contaCorrente.setSaldoEspecial(10000);
-
-contaPoupanca = new Poupanca();
-contaPoupanca.setCorrentista("Rafael Teixeira Lima");
-contaPoupanca.setBanco("Bradesco");
-contaPoupanca.setConta("187778-5");
-contaPoupanca.setSaldo(6780);
-contaPoupanca.setJuros(0.05);
-contaPoupanca.setDataVencimento("10");
-
-alert("CONTA CORRENTE:" +
-  "\nNome Correntista: " + contaCorrente.getCorrentista() +
-  "\nBanco: " + contaCorrente.getBanco() +
-  "\nN° Conta: " + contaCorrente.getConta() +
-  "\nSaldo: " + contaCorrente.getSaldo() +
-  "\nSaldo Especial: " + contaCorrente.getSaldoEspecial());
-
-alert("CONTA POUPANÇA:" +
-  "\nNome Correntista: " + contaPoupanca.getCorrentista() +
-  "\nBanco: " + contaPoupanca.getBanco() +
-  "\nN° Conta: " + contaPoupanca.getConta() +
-  "\nSaldo: " + contaPoupanca.getSaldo() +
-  "\nJuros: " + contaPoupanca.getJuros() +
-  "\nData Vencimento: Todo dia " + contaPoupanca.getDataVencimento());
+console.log(Alunos3)
